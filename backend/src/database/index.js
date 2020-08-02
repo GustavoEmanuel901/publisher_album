4bae0f74ec82939dbf641bb856f7ed21a1621cf8
Gustavo Emanuel <gustavoemanuel901@gmail.com>
2020-08-02 11:21:12 -0300

    Admin connection

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,9 +2,11 @@ const Sequelize = require("sequelize")
 const dbConfig = require('../config/database')
 
 const User = require('../models/User')
+const Admin = require('../models/Admin')
 
 const connection = new Sequelize(dbConfig)
 
 User.init(connection)
+Admin.init(connection)
 
 module.exports = connection
